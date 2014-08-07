eaa98d873aff1cb557352319e6d7e632ee83b21e
chadkila <yuzhaobo77@gmail.com>
2014-08-06 21:08:16 -0700

    Max Points on a Line

--- MaxPointsLine/src/Solution.java
+++ MaxPointsLine/src/Solution.java
@@ -1,8 +1,14 @@
+/**
+ * https://oj.leetcode.com/problems/max-points-on-a-line/
+ * Given n points on a 2D plane, find the maximum number of points that lie on
+ * the same straight line.
+ * 
+ * use a hash table to store numbers of point at each slope
+ * then find maximum one
+ */
 import java.util.*;
 
 /**
- * http://oj.leetcode.com/problems/max-points-on-a-line/
- * 
  * @author yu
  * 
  */
@@ -23,9 +29,6 @@ class Point {
 }
 
 public class Solution {
-	public Solution() {
-
-	}
 
 	HashMap<Float, Integer> slopes = new HashMap<Float, Integer>();
 	int max_points = 0;
@@ -44,15 +47,22 @@ public class Solution {
 		for (int i = 0; i < points.length; i++) {
 			slopes.clear();
 			slopes.put(Float.MIN_VALUE, 0);
+			// duplicated points
 			int duplicate = 1;
 			for (int j = i + 1; j < points.length; j++) {
+				// finding a duplicated point
 				if (points[i].x == points[j].x && points[i].y == points[j].y) {
 					duplicate++;
 					continue;
 				}
+				// sometimes it may give negative 0, see the println(...)
+				// so need to judge if they have same y first
 				if (points[i].y == points[j].y) {
 					float slope = 0;
 					addLinePoint(slope);
+					// System.out.println(((float) points[j].y - (float)
+					// points[i].y)
+					// / ((float) points[j].x - (float) points[i].x));
 				} else {
 					float slope = (float) (points[i].x == points[j].x ? Float.MAX_VALUE
 							: ((float) points[j].y - (float) points[i].y)
