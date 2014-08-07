d4546b8462f11de9cd72a18baafc9e134b2d4bcb
chadkila <yuzhaobo77@gmail.com>
2014-08-06 19:35:54 -0700

    Decode Ways

---
+++ DecodeWays/src/SolutionTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testNumDecodings() {
+		String s = "1102";
+		Solution test = new Solution();
+		System.out.println(test.numDecodings(s));
+	}
+
+}
