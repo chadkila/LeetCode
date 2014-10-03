cea25f89b2ad561eb09752ca5eb3f03bee571abf
chadkila <yuzhaobo77@gmail.com>
2014-10-02 22:21:51 -0700

    Path Sum

---
+++ PathSum/src/SolutionTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testHasPathSum() {
+		TreeNode root = new TreeNode(5);
+		root.left = new TreeNode(4);
+		root.right = new TreeNode(8);
+		root.left.left = new TreeNode(11);
+		root.right.left = new TreeNode(13);
+		root.right.right = new TreeNode(4);
+		root.left.left.left = new TreeNode(7);
+		root.left.left.right = new TreeNode(2);
+		root.right.right.right = new TreeNode(1);
+		Solution test = new Solution();
+		System.out.print(test.hasPathSum(root, 22));
+	}
+
+}
