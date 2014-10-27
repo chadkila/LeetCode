803c184aadacd13a73102a19b440180dcf177b98
chadkila <yuzhaobo77@gmail.com>
2014-10-26 22:44:31 -0700

    Length of Last Word

---
+++ LengthofLastWord/src/SolutionTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void testLengthOfLastWord() {
+		Solution test = new Solution();
+		System.out.println(test.lengthOfLastWord("  Hello World Yu   "));
+		System.out.println(test.lengthOfLastWord("    "));
+	}
+
+}
